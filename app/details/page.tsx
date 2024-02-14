ff0ae5e2cc9d3fd2b923d60ba09efe601d3c4074
Radish007 <@wogus5357@gmail.com>
2024-02-14 15:41:13 +0900

    navBarseparation
    navBarseparation

---
+++ app/details/page.tsx
@@ -0,0 +1,13 @@
+import DetailsPage from "@/components/trade/DetailsPage";
+import { NextPage } from "next";
+
+const Details: NextPage = () => {
+  return (
+    <>
+      <DetailsPage />
+      <div className="max-w-screen h-screen bg-[#E8FFCF] pt-20"></div>
+    </>
+  );
+};
+
+export default Details;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,28 +2,35 @@ import Image from "next/image";
 import NextFarmLogo from "/public/images/NextFarmLogo.png";
 import Link from "next/link";
 import { FC } from "react";
+import NZFToken from "../public/images/NZFToken.png";
+import Fix from "../public/images/fix.png";
 
 const Navbar: FC = () => {
   return (
     <>
-      <div className="max-w-screen bg-[#3E1C4F] h-16">
-        <div className="flex justify-between w-1/2 mx-auto h-full items-center">
-          <div className="flex w-[500px] justify-between items-center pr-5">
-            <Link href={"/"}>
-              <Image src={NextFarmLogo} alt="" className="h-10 w-36" />
-            </Link>
-            <Link href={"/trade"}>
-              <h1>Trade</h1>
-            </Link>
-            <Link href={"/farm"}>
-              <h1>Farm</h1>
-            </Link>
-            <h1>Earn</h1>
-            <h1>...</h1>
-          </div>
-          <div className="flex items-center">
-            <h1>Connect Wallet</h1>
-          </div>
+      <div className="max-w-screen bg-[#E8FFCF] h-10 flex flex-row items-center justify-between">
+        <div className="flex items-center">
+          <Image
+            src={NextFarmLogo}
+            alt="NextFarmLogo"
+            className="h-10 w-10 ml-3 "
+          />
+          <Link href="/">
+            <button className="text-2xl pl-3 text-[#00B050] font-sans">
+              NEXT FARM
+            </button>
+          </Link>
+          <button className="pl-5">Trade</button>
+          <button className="pl-5">Farm</button>
+          <button className="pl-5">Earn</button>
+          <h2>...</h2>
+        </div>
+        <div className="flex items-center gap-3 pr-5">
+          <Image src={NZFToken} alt="NZFToken" className="h-10 w-10 ml-3 " />
+          <h2>$1.095</h2>
+          <Image src={Fix} alt="NZFToken" className="h-7 w-7 ml-3 " />
+          <button>Choose chain</button>
+          <button>Connect Wallet</button>
         </div>
       </div>
     </>
